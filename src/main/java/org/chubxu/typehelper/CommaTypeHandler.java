58927c94d79da53afbbd8a288b5be11a119167c8
chubxu <chubxu@gmail.com>
2022-09-28 21:21:59 +0800

    feat: 支持中文逗号、句号自动转变英文逗号、句号

--- src/main/java/org/chubxu/typehelper/CommaTypeHandler.java
+++ src/main/java/org/chubxu/typehelper/CommaTypeHandler.java
@@ -10,7 +10,19 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommaTypeHandler extends TypedHandlerDelegate {
+    // 中文标点 -> 英文标点
+    private static final Map<Character, Character> chPunToEnPun;
+
+    static {
+        chPunToEnPun = new HashMap<>();
+        chPunToEnPun.put('，', ',');
+        chPunToEnPun.put('。', '.');
+    }
+
     @Override
     public @NotNull Result beforeCharTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file, @NotNull FileType fileType) {
         return Result.CONTINUE;
@@ -18,14 +30,14 @@ public class CommaTypeHandler extends TypedHandlerDelegate {
 
     @Override
     public @NotNull Result charTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
-        String commaChinese = String.valueOf(c);
-        if ("，".equals(commaChinese)) {
+        if (chPunToEnPun.containsKey(c)) {
             Document document = editor.getDocument();
             CaretModel caretModel = editor.getCaretModel();
             Caret primaryCaret = caretModel.getPrimaryCaret();
+
             int caretOffset = primaryCaret.getOffset();
             document.deleteString(caretOffset-1, caretOffset);
-            document.insertString(caretOffset-1, ",");
+            document.insertString(caretOffset-1, String.valueOf(chPunToEnPun.get(c)));
             primaryCaret.moveToOffset(caretOffset);
         }
         return Result.CONTINUE;
