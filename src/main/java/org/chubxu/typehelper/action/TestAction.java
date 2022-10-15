804d29ca8c15126f91f95f0e88e85270a4a8ced4
chubxu <chubxu@gmail.com>
2022-10-15 12:38:42 +0800

    opt: 优化禅定模式的显示区域以及高亮颜色

--- src/main/java/org/chubxu/typehelper/action/TestAction.java
+++ src/main/java/org/chubxu/typehelper/action/TestAction.java
@@ -2,65 +2,60 @@ package org.chubxu.typehelper.action;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.*;
+import com.intellij.openapi.editor.markup.HighlighterLayer;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.MarkupModel;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.ui.Gray;
-import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
+import java.util.Objects;
 
 public class TestAction extends DumbAwareAction {
 
-    private TextAttributes textAttributes;
+    private final TextAttributes textAttributes;
+
+    public TestAction() {
+        super();
+        textAttributes = new TextAttributes();
+        textAttributes.setBackgroundColor(Gray._51);
+    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
         Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-        PsiFile psiFile = e.getRequiredData(CommonDataKeys.PSI_FILE);
-        String selectedText = editor.getSelectionModel().getSelectedText();
-        textAttributes = new TextAttributes(JBColor.YELLOW, JBColor.BLUE, JBColor.GRAY, EffectType.ROUNDED_BOX, Font.BOLD);
-
-        PsiElement psiElement = psiFile.findElementAt(editor.getCaretModel().getOffset());
-        PsiElement elementParent = psiElement.getParent();
-
-
-        PsiMethod psiMethod = PsiTreeUtil.findElementOfClassAtOffset(
-                psiFile,
-                editor.getCaretModel().getOffset(),
-                PsiMethod.class,
-                false
-        );
-        TextRange textRange = psiMethod.getTextRange();
-
-
-//        TextRange textRange = new TextRange(
-//                editor.getSelectionModel().getSelectionStart(),
-//                editor.getSelectionModel().getSelectionEnd()
-//        );
-
-
         MarkupModel markupModel = editor.getMarkupModel();
+        PsiFile psiFile = e.getRequiredData(CommonDataKeys.PSI_FILE);
 
+        PsiMethod psiMethod = PsiTreeUtil.findElementOfClassAtOffset(psiFile, editor.getCaretModel().getOffset(),
+                PsiMethod.class, false);
+
+        if (Objects.isNull(psiMethod)) {
+            markupModel.removeAllHighlighters();
+            return ;
+        }
         markupModel.removeAllHighlighters();
+        TextRange textRange = psiMethod.getTextRange();
+        markupModel.addRangeHighlighter(0, getLineStartOffsetByMethodStartOffset(editor, textRange.getStartOffset()),
+                HighlighterLayer.FIRST, textAttributes, HighlighterTargetArea.EXACT_RANGE);
+        markupModel.addRangeHighlighter(getLineEndOffsetByMethodEndOffset(editor, textRange.getEndOffset()),
+                editor.getDocument().getTextLength(), HighlighterLayer.SELECTION - 1, textAttributes,
+                HighlighterTargetArea.EXACT_RANGE);
+    }
 
-        markupModel.addRangeHighlighter(
-                textRange.getStartOffset(),
-                textRange.getEndOffset(),
-                HighlighterLayer.SELECTION - 1,
-                textAttributes,
-                HighlighterTargetArea.EXACT_RANGE
-        );
+    private int getLineStartOffsetByMethodStartOffset(Editor editor, int startOffset) {
+        int lineNumber = editor.getDocument().getLineNumber(startOffset);
+        return editor.getDocument().getLineStartOffset(lineNumber);
+    }
+
+    private int getLineEndOffsetByMethodEndOffset(Editor editor, int endOffset) {
+        int lineNumber = editor.getDocument().getLineNumber(endOffset);
+        return editor.getDocument().getLineStartOffset(lineNumber+1);
     }
 }
